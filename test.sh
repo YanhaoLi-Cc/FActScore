f8f567ee9a2484a76495d1e608bbb26f2d00ee5a
YanhaoLi-Cc <1310724226@qq.com>
2025-03-11 21:54:33 +0800

    fix bash

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-# python3 -m factscore.factscorer --input_path /home/liyanhao/FActScore/data/unlabeled/DeepSeek-R1-Distill-Qwen-7B.jsonl --model_name retrieval+llama+npm
+# python3 -m factscore.factscorer --input_path /home/liyanhao/FActScore/data/unlabeled/DeepSeek-R1.jsonl --model_name retrieval+ChatGPT --openai_key /home/liyanhao/FActScore/openai_key.txt --verbose
 
-python3 -m factscore.factscorer --input_path /home/liyanhao/FActScore/data/unlabeled/DeepSeek-R1-Distill-Qwen-7B-demo.jsonl --model_name retrieval+ChatGPT --openai_key /home/liyanhao/FActScore/openai_key.txt
+# echo "等待10分钟..."
+# sleep 600
+
+python3 -m factscore.factscorer --input_path /home/liyanhao/FActScore/data/unlabeled/DeepSeek-V3.jsonl --model_name retrieval+ChatGPT --openai_key /home/liyanhao/FActScore/openai_key.txt --verbose
